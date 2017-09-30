ce335a9e4c2787e0d55f92bfc9419ef5a233029e
ram-cs <ramusa2011@ucla.edu>
2017-09-30 16:03:57 -0700

    websites

---
+++ file.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html> 
+<html lang="en">
+<meta charset="utf-8">
+<meata name="viewport" contain="width=device-width, initial-scale=1.0">
+
+<body>
+<?php
+	echo "my first turtorial";
+?>
+</body>
+
+</html>
